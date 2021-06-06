db68044f70f810f654268ddf7380124606b10209
magne-matias <magnematias01@gmail.com>
2021-06-06 20:50:56 -0300

    commit

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ document.getElementById('formTask').addEventListener('submit', saveTask);
 //el documento selecciona formTask. y cuando se presiona el evento submit, ejecuta la funcion saveTask
 
 
-function saveTask(){
-    
-    e.preventDefault();//evita que se refresque la pagina cuando le das a confirm
-
+function saveTask(e){
     let title = document.getElementById('title').value;//de esta menera logramos ingresar a que es lo que se escribe en ADD TASK
     //esto necesita ser una constante para ser reutilizable en el resto de nuestra app
 
@@ -16,4 +13,28 @@ function saveTask(){
         description // description:description
     };
 
+    /*localStorage.setItem('tasks',JSON.stringify(task));//nos permite poder almacenar un dato pero necesitamos darle dos parametros, todo queda almacenado en localstorage
+    //(nombre, valor del dato ) */
+
+    localStorage.setItem('tasks',JSON.stringify(task));//para convertir un objeto a un string podemos utilizar JSON.stringify, este metodo nos permite convertir un objeto en un string
+    //es mejor tenerlos almacenados en formato string
+    localStorage.getItem('tasks')//para obtener los datos de arriba, solamente necesitamos pedir el dato
+
+
+    if(localStorage.getItem('tasks')===null){//si desde el localstorage ya existe un valor llamado tareas y es igual a nulo vamos a crear tareas
+        let task=[];
+        tasks.push()//para a llenarlo con el metodo push
+        
+        //almacenarlo en el localstorage
+        localStorage.setItem('tasks',JSON.stringify(task));
+        
+    }else{//si ya existe valores vamos a empezar a actualizarlos
+
+    }
+
+
+
+    e.preventDefault();//evita que se refresque la pagina cuando le das a confirm
+
+    //todos nuestros datos van a quedar guardados en localStorage
 }
